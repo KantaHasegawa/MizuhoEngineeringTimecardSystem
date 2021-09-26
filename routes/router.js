bd55e51e5caf5d4c439b294361f5e6a12e1e1211
KantaHasegawa <gyoza20000@gmail.com>
2021-09-26 18:11:33 +0900

    connect dynamodb

--- routes/router.js
+++ routes/router.js
@@ -1,11 +1,29 @@
 const express = require("express");
 const router = express.Router();
+const AWS = require("aws-sdk");
+
+const dynamoOptions =
+  process.env.NODE_ENV === "development"
+    ? {
+        region: "localhost",
+        endpoint: "http://localhost:8000",
+      }
+    : {};
+const documentClient = new AWS.DynamoDB.DocumentClient(dynamoOptions);
 
 router.get("/", (req, res) => {
-  res.json({ message: "Hello World! root directory!!" });
+  const env = process.env.NODE_ENV
+  res.json({ message: `env is ${env}` });
 });
+
 router.get("/users", (req, res) => {
-  res.json({message: "user list!!"});
+  documentClient
+    .scan({
+      TableName: "Timecards",
+    })
+    .promise()
+    .then((result) => res.json(result))
+    .catch((e) => res.status(422).json({ errors: e }));
 });
 
 module.exports = router;
