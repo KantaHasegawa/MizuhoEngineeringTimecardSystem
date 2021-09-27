b8ee0e9d998e3e919913db979ef17c52caeb31c6
KantaHasegawa <gyoza20000@gmail.com>
2021-09-27 15:28:24 +0900

    add user show&index

--- routes/v1/user.js
+++ routes/v1/user.js
@@ -7,27 +7,46 @@ const jwt = require('jsonwebtoken');
 const dynamoOptions =
   process.env.NODE_ENV === "development"
     ? {
-        region: "localhost",
-        endpoint: "http://localhost:8000",
-      }
+      region: "localhost",
+      endpoint: "http://localhost:8000",
+    }
     : {};
 const documentClient = new AWS.DynamoDB.DocumentClient(dynamoOptions);
 
-router.get("/index", (req, res) => {
-  documentClient
-    .scan({
+  router.get("/show/:name", (req, res) => {
+    const params = {
       TableName: "Timecards",
+      Key: {
+        user: req.params.name,
+        workspot: "user"
+      }
+    };
+    documentClient.get(params).promise()
+      .then((result) => res.json(result.Item))
+      .catch((e) => res.status(500).json({ errors : e}))
+  })
+
+router.get("/index", (req, res) => {
+    const params = {
+      TableName: 'Timecards',
+      IndexName: 'usersIndex',
+      ExpressionAttributeNames: { '#w': 'workspot' },
+      ExpressionAttributeValues: { ':val': 'user' },
+      KeyConditionExpression: '#w = :val'
+    };
+  documentClient.query(params, (err, result) => {
+    if (err) {
+      res.status(500).json({errors: e})
+    } else {
+      res.json(result.Items)
+      }
     })
-    .promise()
-    .then((result) => res.json(result))
-    .catch((e) => res.status(422).json({ errors: e }));
 });
 
 router.post("/signup", async (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
   const hashedPassword = await bcrypt.hash(password, 10);
-  console.log(hashedPassword);
 
   const params = {
     user: username,
@@ -43,7 +62,7 @@ router.post("/signup", async (req, res) => {
     })
     .promise()
     .then((result) => res.json({ message: "insert seccess" }))
-    .catch((e) => res.status(422).json({ errors: e }));
+    .catch((e) => res.status(500).json({ errors: e }));
 });
 
 function authenticateToken(req, res, next) {
