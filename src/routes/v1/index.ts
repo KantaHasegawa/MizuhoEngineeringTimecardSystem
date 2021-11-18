900b219b6a822cdf3b779ed8cbea37b2e053a335
KantaHasegawa <gyoza20000@gmail.com>
2021-11-18 21:29:05 +0900

    refactor: Remove Debug route

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,32 +1,10 @@
 import express from "express";
 const router = express.Router();
-import documentClient from "../../helper/dbconnect";
 import userRoute from "./userRouter";
 import authRoute from "./authRouter";
 import workspotRoute from "./workspotRouter";
 import timecardRoute from "./timecardRouter";
 import relationRoute from "./relationRouter";
-
-router.get(
-  "/api/v1/",
-  (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    const env: string = process.env.NODE_ENV ?? "development";
-    res.json({ message: `env is ${env}` });
-  }
-);
-
-router.get(
-  "/api/v1/records",
-  (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    documentClient
-      .scan({
-        TableName: "Timecards",
-      })
-      .promise()
-      .then((result) => res.json(result))
-      .catch((err) => next(err));
-  }
-);
 
 router.use("/api/v1/user", userRoute);
 router.use("/api/v1/auth", authRoute);
